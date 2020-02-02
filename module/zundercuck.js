2fb35e9ef201681807a1c85d04e471c6f9f6c48a
Jason McKinney <jason@jmmckinney.net>
2020-02-02 18:36:58 -0500

    Add roll calculator Add default attributes to character template Add custom roll parser Increment version to 0.1.1

--- module/zundercuck.js
+++ module/zundercuck.js
@@ -9,7 +9,8 @@ import { ZunActorSheet } from "./actor-sheet.js";
 Hooks.once("init", async function() {
   console.log(`ZUNDERCUCK!`);
 
-  game.getattribute = getattribute;
+  game.getAttributeValue = getAttributeValue;
+  game.zundercuckRoll = zundercuckRoll;
 
 	/**
 	 * Set an initiative formula for the system
@@ -22,65 +23,142 @@ Hooks.once("init", async function() {
   Actors.registerSheet("zundercuck", ZunActorSheet, { makeDefault: true });
   Items.unregisterSheet("core", ItemSheet);
   Items.registerSheet("zundercuck", ZunItemSheet, {makeDefault: true});
+  
 });
 
+//initiative command
 Hooks.on("chatMessage", (chatlog, message, chatData) => {
-  const speaker = ChatMessage.getSpeaker();
-  const actor = game.actors.get(speaker.actor);
-  const token = canvas.tokens.get(speaker.token);
-  const character = game.user.character;
+  let [command, m] = parse(message);
 
-  let [command, m] = chatlog.constructor.parse(message);
   switch (command) {
-    case "roll": case "gmroll": case "blindroll": case "selfroll":
-      var formula = message.replace(/\/roll|\/gmroll|\/blindroll|\/selfroll/, "").trim();
-      
-      //actor attribute
-      var match = formula.match(/\@[^+\-\/*\s]*/g);
-      if (match) {
-        match.forEach((attr) => {
-          console.log (attr.substring(1));
-          const value = actor ? getattribute(actor, attr.substring(1)) : 0;
-          console.log(value);
-          formula = formula.replace(attr, value);
-        });
-      }
-      
-      //token attribute
-      match = formula.match(/\$[^+\-\/*\s]*/g);
-      if (match) {
-        match.forEach((attr) => {
-          console.log (attr.substring(1));
-          const value = token ? getattribute(token.actor, attr.substring(1)) : 0;
-          console.log(value);
-          formula = formula.replace(attr, value);
-        });
-      }
+    case "initiative":
+      const token = canvas.tokens.get(ChatMessage.getSpeaker().token);
+      if (!token) { break; }
 
-      //character attribute
-      match = formula.match(/\&[^+\-\/*\s]*/g);
-      if (match) {
-        match.forEach((attr) => {
-          console.log (attr.substring(1));
-          const value = character ? getattribute(character, attr.substring(1)) : 0;
-          console.log(value);
-          formula = formula.replace(attr, value);
-        });
-      }
-
-      console.log(formula);
-      const roll = new Roll(formula, {});
-      console.log(roll);
-      roll.roll();
-      roll.toMessage({rollMode: command});
+      var attr = message.replace(/\/i(?:nitiative)?/, "").trim();
+      attr = getAttributeValue(token.actor, attr);
       
+      game.combat.rollInitiative(game.combat.getCombatantByToken(token.id)._id, CONFIG.initiative.formula + "+" + attr);
+
       return false;
   }
 
   return true;
 });
 
-function getattribute(actor, attribute) {
+//dice roller attribute parser
+Hooks.on("chatMessage", (chatlog, message) => {
+  let [command, m] = parse(message);
+  switch (command) {
+    case "roll": case "gmroll": case "blindroll": case "selfroll":
+      var formula = message.replace(/\/r(?:oll)?|\/gmr(?:oll)|\/b(?:lind)?r(?:oll)?|\/s(?:elf)?r(?:oll)?/, "").trim();
+      
+      zundercuckRoll(formula, command);
+      return false;
+  }
+
+  return true;
+});
+
+function getAttributeValue(actor, attribute) {
   const attr = actor.data.data.attributes[attribute];
   return attr ? attr.value : 0;
+}
+
+function zundercuckRoll (formula, rollMode = "roll") {
+  const speaker = ChatMessage.getSpeaker();
+  const actor = game.actors.get(speaker.actor);
+  const token = canvas.tokens.get(speaker.token);
+  const character = game.user.character;
+
+  var match = formula.match(/\d*d\d+h/g);
+  if (match) {
+    match.forEach((m) => {
+      const groups = /(\d*)d(\d+)h/.exec(m);
+      const left = groups[1]>1 ? (groups[1]-1 + "" + "d" + groups[2] + "+") : "";
+      formula = formula.replace(groups[0], left + "abs(" + "1d" + groups[2] + "-" + "1d" + groups[2] + ")");
+    });
+  }
+
+  match = formula.match(/\d*d\d+e/g);
+  if (match) {
+    match.forEach((m) => {
+      const groups = /(\d*d(\d+))e/.exec(m);
+      formula = formula.replace(groups[0], groups[1] + "+1d" + groups[2]);
+    });
+  }
+
+  //actor attribute
+  match = formula.match(/\@[^+\-\/*\s]*/g);
+  if (match) {
+    match.forEach((attr) => {
+      const value = actor ? getAttributeValue(actor, attr.substring(1)) : 0;
+      formula = formula.replace(attr, value);
+    });
+  }
+  
+  //token attribute
+  match = formula.match(/\$[^+\-\/*\s]*/g);
+  if (match) {
+    match.forEach((attr) => {
+      const value = token ? getAttributeValue(token.actor, attr.substring(1)) : 0;
+      formula = formula.replace(attr, value);
+    });
+  }
+
+  //character attribute
+  match = formula.match(/\&[^+\-\/*\s]*/g);
+  if (match) {
+    match.forEach((attr) => {
+      const value = character ? getAttributeValue(character, attr.substring(1)) : 0;
+      formula = formula.replace(attr, value);
+    });
+  }
+  
+  const roll = new Roll(formula, {});
+  roll.roll();
+  roll.toMessage({}, {rollMode:rollMode, create:true});
+
+  if (!actor && formula.includes('@')) {
+    ui.notifications.warn('A token attribute was specified in your roll, but no token was selected.');
+  }
+}
+
+function parse(message) {
+  // Dice roll regex
+  let formula = '([^#]*)';                  // Capture any string not starting with '#'
+  formula += '(?:(?:#\\s?)(.*))?';          // Capture any remaining flavor text
+  const roll = '^(\\/r(?:oll)? )';          // Regular rolls, support /r or /roll
+  const gm = '^(\\/gmr(?:oll)? )';          // GM rolls, support /gmr or /gmroll
+  const br = '^(\\/b(?:lind)?r(?:oll)? )';  // Blind rolls, support /br or /blindroll
+  const sr = '^(\\/s(?:elf)?r(?:oll)? )';   // Self rolls, support /sr or /sroll
+  const initiative = '^(\\/i(?:nitiative)? )';
+  const any = '([^]*)';                     // Any character, including new lines
+  const word = '\\S+';
+
+  // Define regex patterns
+  const patterns = {
+    "roll": new RegExp(roll+formula, 'i'),
+    "gmroll": new RegExp(gm+formula, 'i'),
+    "blindroll": new RegExp(br+formula, 'i'),
+    "selfroll": new RegExp(sr+formula, 'i'),
+    "initiative": new RegExp(initiative+word+'$', 'i'),
+    "ic": new RegExp('^(\/ic )'+any, 'i'),
+    "ooc": new RegExp('^(\/ooc )'+any, 'i'),
+    "emote": new RegExp('^(\/em(?:ote)? )'+any, 'i'),
+    "whisper": new RegExp(/^(@|\/w(?:hisper)?\s{1})(\[(?:[^\]]+)\]|(?:[^\s]+))\s+([^]*)/, 'i'),
+    "none": new RegExp('()'+any, 'i')
+  };
+
+  // Iterate over patterns, finding the first match
+  let c, rgx, match;
+  for ( [c, rgx] of Object.entries(patterns) ) {
+    match = message.match(rgx); 
+    if ( match ) return [c, match];
+  }
+  return [null, null];
+}
+
+export const _onCombatantControl = function (event) {
+  console.log("Bip!");
 }
---
+++ scripts/dice-calculator.js
@@ -0,0 +1,332 @@
+$(document).ready(() => {
+  'use strict';
+
+  // eslint-disable-next-line max-len
+  const templatePath = 'systems/zundercuck/templates/calculator.html';
+  const diceIconSelector = '#chat-controls .roll-type-select .fa-dice-d20';
+
+  /**
+   * Helper function to roll dice formula and output to chat.
+   *
+   * @param {object} actor (optional)
+   *   The actor to use for rolls, if any.
+   *
+   * @return {undefined}
+   */
+  function dcRollDice(actor = false) {
+    let formula = $('.dice-calculator textarea').val();
+    game.zundercuckRoll(formula);
+  }
+
+  // Render a modal on click.
+  $(document).on('click', diceIconSelector, ev => {
+    ev.preventDefault();
+
+    let $dialog = $('.dialog--dice-calculator');
+
+    if ($dialog.length < 1) {
+
+      const dialogOptions = {
+        width: 400,
+        top: event.clientY - 80,
+        left: window.innerWidth - 710,
+        classes: ['dialog', 'dialog--dice-calculator']
+      };
+
+      let token = canvas.tokens.get(ChatMessage.getSpeaker().token);
+      let actor = token ? token.actor : undefined;
+
+      let abilities = false;
+      let attributes = game.system.template.Actor.character.attributes;
+      let customButtons = {
+        easy: {
+          name: "easy",
+          label: "easy",
+          formula: "e"
+        },
+        hard: {
+          name: "hard",
+          label: "hard",
+          formula: "h"
+        }
+      };
+
+      // Build the template.
+      let templateData = {
+        'abilities': abilities,
+        'attributes': attributes,
+        'customButtons': customButtons,
+        'adv': false
+      };
+
+      // Render the modal.
+      renderTemplate(templatePath, templateData).then(dlg => {
+        new Dialog({
+          title: 'Roll Dice',
+          content: dlg,
+          buttons: {
+            roll: {
+              label: 'Roll',
+              callback: () => dcRollDice(actor)
+            }
+          }
+        }, dialogOptions).render(true);
+      });
+    }
+    else {
+      $dialog.remove();
+    }
+  });
+
+  // Toggle the memory display.
+  $(document).on('focus', '.dice-calculator__text-input', function(event) {
+    $('.dice-calculator__rolls--hidden').addClass('visible');
+  });
+
+  $(document).on('blur', '.dice-calculator__text-input', function(event) {
+    setTimeout(() => {
+      $('.dice-calculator__rolls--hidden').removeClass('visible');
+    }, 250);
+  });
+
+  // Replace with the selected die formula.
+  $(document).on('click', '.dice-calculator__roll', function(event) {
+    let formula = $(this).text();
+    $('.dice-calculator__text-input').val(formula);
+  });
+
+  // Update the dice formula.
+  $(document).on('click', '.dice-calculator--button', ev => {
+    ev.preventDefault();
+
+    // Retrieve variables to start things off.
+    let buttonFormula = String($(ev.currentTarget).data('formula'));
+    let $formulaInput = $(document).find('.dice-calculator textarea');
+    let currentFormula = $formulaInput.val();
+    let currentFormulaArray = currentFormula.split(' ');
+    let last = currentFormulaArray.slice(-1)[0];
+    let skip = false;
+
+    // Used for determining when to add directly.
+    const standardDice = [
+      'd3',
+      'd4',
+      'd6',
+      'd8',
+      'd10',
+      'd12',
+      'd20',
+      'd30',
+      'd100'
+    ];
+
+    // Used to prevent doubling up on operations/symbols.
+    const opFormulas = [
+      '/',
+      '*',
+      '+',
+      '-'
+    ];
+    const parenthesesFormulas = [
+      '(',
+      ')'
+    ];
+
+    // If the last item and the incoming item are both math operations,
+    // skip adding it.
+    if (opFormulas.includes(last) && opFormulas.includes(buttonFormula)) {
+      skip = true;
+    }
+
+    // If the incoming item is either an operation or parentheses, skip it
+    // in certain cases.
+    if (opFormulas.includes(buttonFormula)
+    || parenthesesFormulas.includes(buttonFormula)) {
+      if (last == buttonFormula) {
+        skip = true;
+      }
+
+      if (opFormulas.includes(last) || parenthesesFormulas.includes(last)) {
+        skip = true;
+      }
+    }
+
+    // Handle clear.
+    if (buttonFormula === 'CLEAR') {
+      currentFormula = '';
+    }
+    // Handle backspace/delete.
+    else if (buttonFormula === 'DELETE') {
+      currentFormulaArray.pop();
+      currentFormula = currentFormulaArray.join(' ');
+    }
+    // Handle token attributes and abilities.
+    else if (last.includes('@') || buttonFormula.includes('@')) {
+      let joiner = opFormulas.includes(last) || opFormulas.includes(buttonFormula) ? ' ' : ' + ';
+      joiner = last.length < 1 ? '' : joiner;
+      currentFormula = currentFormula + joiner + buttonFormula;
+    }
+    // Handle inputs such as 'd4' and 'd6'
+    else if (buttonFormula.includes('d')) {
+      let updated = false;
+      let count = 0;
+      // @TODO: Investigate whether this option is worthwhile, perhaps as a
+      // a setting.
+      // -----------------------------------------------------------------------
+      // Loop through all existing items in the formula array to see if this
+      // die type has been used before. If so, increase the count on that entry.
+      // for (let i = 0; i < currentFormulaArray.length; i++) {
+      //   if (currentFormulaArray[i].includes(buttonFormula)) {
+      //     let result = currentFormulaArray[i].split('d');
+      //     if (result[0] && (result[0].length !== 0 || !isNaN(result[0]))) {
+      //       count = parseInt(result[0]);
+      //     }
+      //     else {
+      //       count = 1;
+      //     }
+      //     updated = true;
+      //     currentFormulaArray[i] = (count + 1) + buttonFormula;
+      //   }
+      // }
+
+      // Update the number of dice in the last item if it's the same as the
+      // button formula.
+      if (last.includes(buttonFormula)) {
+        let result = last.split('d');
+        if (result[0] && (result[0].length !== 0 || !isNaN(result[0]))) {
+          count = parseInt(result[0]);
+        }
+        else {
+          count = 1;
+        }
+        updated = true;
+        currentFormulaArray[currentFormulaArray.length - 1] = (count + 1) + buttonFormula;
+      }
+      // If we updated an item, create a new text version of the formula.
+      if (updated) {
+        currentFormula = currentFormulaArray.join(' ');
+      }
+      // Otherwise, append to the original.
+      else {
+        let joiner = ' ';
+        if (last.includes('d')) {
+          joiner = ' + ';
+        }
+        else if (!isNaN(last)) {
+          joiner = '';
+        }
+        currentFormula = currentFormula + joiner + buttonFormula;
+      }
+    }
+    // Handle adv/dis.
+    else if (buttonFormula.includes('k')) {
+      if (last.includes('d')) {
+        // If the last item isn't kh or kl, append.
+        let lastArray = last.split('k');
+        if (!last.includes('k')) {
+          if (last === 'd20') {
+            last = '2d20';
+          }
+
+          currentFormula = currentFormula.slice(0, -1 * last.length);
+          currentFormula = currentFormula + ' ' + last + buttonFormula;
+        }
+        // Otherwise check to see if we should either replace (such as going
+        // from kh to kl) or increase the count.
+        else {
+          if (lastArray[1]) {
+            let dieCount = lastArray[0].split('d')[0];
+            let lastType = lastArray[1].substr(0, 1);
+            let buttonType = buttonFormula.substr(1, buttonFormula.length);
+            let lastCount = lastArray[1].substr(1, lastArray[1].length);
+            let count = parseInt(lastCount);
+            dieCount = parseInt(dieCount);
+            dieCount = isNaN(dieCount) ? 1 : dieCount;
+            count = isNaN(count) ? 2 : count + 1;
+
+            // We need to avoid allowing more dice to be kept than are being
+            // rolled.
+            if (count >= dieCount) {
+              count = dieCount - 1;
+            }
+
+            if (count === 1) {
+              count = '';
+            }
+
+            // Build the new string.
+            currentFormulaArray.pop();
+            currentFormula = currentFormulaArray.join(' ') + lastArray[0]
+              + 'k' + buttonType;
+
+            if (buttonType === lastType) {
+              currentFormula = currentFormula + count;
+            }
+          }
+        }
+      }
+      else {
+        skip = true;
+      }
+    }
+    //handle easy and hard
+    else if (buttonFormula === 'e' || buttonFormula === 'h') {
+      if (last.includes('d')) {
+        if (last.includes('e') || last.includes('h')) {
+          skip = true;
+        } else {
+          currentFormula = currentFormula.slice(0, -1 * last.length);
+          currentFormula = currentFormula + ' ' + last + buttonFormula;
+        }
+      } else {
+        skip = true;
+      }
+    }
+    // Handle custom dice.
+    else if (buttonFormula === 'd') {
+      let joiner = last.includes('d') || isNaN(last) ? ' + ' : '';
+      currentFormula = currentFormula + joiner + buttonFormula;
+    }
+    // Handle numbers appended to custom dice..
+    else if ((last === 'd' || last.includes('k')
+    || parenthesesFormulas.includes(last)) && !isNaN(buttonFormula)) {
+      currentFormula = currentFormula + buttonFormula;
+    }
+    // All other inputs.
+    else {
+      // Normally we want to append with either an empty string or a space.
+      let joiner = '';
+      if (currentFormula.length === 0) {
+        joiner = '';
+      }
+      if (opFormulas.includes(buttonFormula)) {
+        joiner = ' ';
+      }
+      // If the last item is a die, append with as addition.
+      else if (last.includes('d')) {
+        if (standardDice.includes(last) && buttonFormula !== '0') {
+          joiner = ' + ';
+        }
+        // If the last item isn't a complete die (such as "3d") and this is
+        // a number, append directly.
+        else {
+          if (!isNaN(last) || !isNaN(parseInt(buttonFormula))) {
+            joiner = '';
+          }
+          else {
+            joiner = ' ';
+          }
+        }
+      }
+      else if (isNaN(parseInt(last))) {
+        joiner = ' ';
+      }
+      currentFormula = currentFormula + joiner + buttonFormula;
+    }
+
+    // If no operations said to skip this entry, update the input.
+    if (!skip) {
+      $formulaInput.val(currentFormula);
+    }
+  });
+});
